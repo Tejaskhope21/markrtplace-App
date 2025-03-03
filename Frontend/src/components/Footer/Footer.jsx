e7b6ade92bfb18b179195d99ed1cd67aec4b620f
YadneshUmredkar@75 <yadneshumredkar75@gmail.com>
2025-03-03 10:53:00 +0530

    footer

--- Frontend/src/components/Footer/Footer.jsx
+++ Frontend/src/components/Footer/Footer.jsx
@@ -1,59 +1,58 @@
 import React from 'react';
-import './Footer.css';
-// import UpperFooter from '../UpperFooter/UpperFooter';
+import './Footer.css'; // Import the CSS file for styling
 
-function App() {
-  return (
-    <div id="root">
-      {/* Main content */}
-      {/* <div className="main-content">
-        {/* <h1>Welcome to MarketSetu</h1>
-        <p>This is the main content of the page.</p> */}
-        {/* <UpperFooter/>
-      </div> */ }
-     {/* } */}
+const Footer = () => {
+    return (
+        <footer className="footer">
+            <div className="footer-content">
+                <div className="footer-column">
+                    <h4>Help</h4>
+                    <ul>
+                        <li><a href="#">Sell on Udsan</a></li>
+                    </ul>
+                </div>
+                <div className="footer-column">
+                    <h4>Company</h4>
+                    <ul>
+                        <li><a href="#">Careers</a></li>
+                        <li><a href="#">About Us</a></li>
+                        <li><a href="#">News Room</a></li>
+                        <li><a href="#">Contact Us</a></li>
+                        <li><a href="#">Regulatory</a></li>
+                    </ul>
+                </div>
+                <div className="footer-column">
+                    <h4>Policies</h4>
+                    <ul>
+                        <li><a href="#">Terms of Use</a></li>
+                        <li><a href="#">Privacy Policy</a></li>
+                        <li><a href="#">IP Infringement Policy</a></li>
+                        <li><a href="#">Anti Counterfeiting Policy</a></li>
+                    </ul>
+                </div>
+                <div className="footer-column">
+                    <h4>Other Policies</h4>
+                    <ul>
+                        <li><a href="#">Udsan Coin Policy</a></li>
+                        <li><a href="#">Returns Policy</a></li>
+                        <li><a href="#">Product Listing Policy</a></li>
+                        <li><a href="#">Undelivered Shipment (RTO) Policy</a></li>
+                    </ul>
+                </div>
+                <div className="footer-column">
+                    <h4>Connect</h4>
+                    <ul>
+                        <li><a href="#">Facebook</a></li>
+                        <li><a href="#">Twitter</a></li>
+                        <li><a href="#">Blog</a></li>
+                    </ul>
+                </div>
+            </div>
+            <div className="footer-bottom">
+                <p>&copy; 2023 Udsan. All rights reserved.</p>
+            </div>
+        </footer>
+    );
+};
 
-      {/* Footer */}
-      <footer className="footer-container">
-        <div className="footer__logo">
-          <img src="/logo.png" alt="Logo" width="50" height="50" />
-          <p>MarketSetu</p>
-          <div>
-            <img src="/apple.png" alt="App Store" width="30" height="30" />
-            <img src="/playstore.png" alt="Google Play" width="30" height="30" />
-          </div>
-        </div>
-
-        <div className="footer__links">
-          <ul>
-            <li><a href="#">Help</a></li>
-            <li><a href="#">Sell on Udaan</a></li>
-            <li><a href="#">Careers</a></li>
-            <li><a href="#">About Us</a></li>
-            <li><a href="#">News Room</a></li>
-          </ul>
-        </div>
-
-        <div className="footer__links">
-          <ul>
-            <li><a href="#">Terms of Use</a></li>
-            <li><a href="#">Privacy Policy</a></li>
-            <li><a href="#">Returns Policy</a></li>
-            <li><a href="#">Product Listing Policy</a></li>
-            <li><a href="#">IP Infringement Policy</a></li>
-          </ul>
-        </div>
-
-        <div className="footer__links">
-          <ul>
-            <li><a href="#">Facebook</a></li>
-            <li><a href="#">Twitter</a></li>
-            <li><a href="#">Blog</a></li>
-          </ul>
-        </div>
-      </footer>
-    </div>
-  );
-}
-
-export default App;
+export default Footer;
