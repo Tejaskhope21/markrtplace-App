d47b4a127a9a9e2dfa6368ef3c206a7b52960908
YadneshUmredkar@75 <yadneshumredkar75@gmail.com>
2025-03-03 11:21:13 +0530

    Buynow add

---
+++ Frontend/src/components/Buynow/Buynow.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './Buynow'
+function Buynow() {
+  return (
+   
+<div class="parent">
+    <div class="div1">1</div>
+    <div class="div2">2</div>
+    <div class="div3">3</div>
+</div>
+    
+  )
+}
+
+export default Buynow
