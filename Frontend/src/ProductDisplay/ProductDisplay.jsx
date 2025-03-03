66f71f03dc9048c14fb0e3bb69a765bf9d796034
Tejaskhope21 <tejaskhope600@gmail.com>
2025-03-03 11:20:50 +0530

    product up

--- Frontend/src/ProductDisplay/ProductDisplay.jsx
+++ Frontend/src/ProductDisplay/ProductDisplay.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import "./ProductDisplay.css";
 import { categories, products } from "../Tempdata.js";
+import { Link } from "react-router-dom";
 
 const ProductDisplay = () => {
   return (
@@ -15,6 +16,7 @@ const ProductDisplay = () => {
         </div>
 
         {/* Category Section */}
+
         <div className="category-container">
           <h2>Source by category</h2>
           <div className="category-grid">
@@ -32,24 +34,26 @@ const ProductDisplay = () => {
         </div>
       </div>
 
-      <div className="product-grid">
-        {products.map((product) => (
-          <div key={product.id} className="product-card">
-            <img
-              src={product.image}
-              alt={product.name}
-              className="product-image"
-            />
-            <h3 className="product-name">{product.name}</h3>
-            <p className="product-price">
-              <span className="current-price">₹{product.price}</span>
-            </p>
-            <p className="product-rating">
-              ⭐ {product.rating} ({product.reviews})
-            </p>
-          </div>
-        ))}
-      </div>
+      <Link className="links" to="/productdetails">
+        <div className="product-grid">
+          {products.map((product) => (
+            <div key={product.id} className="product-card">
+              <img
+                src={product.image}
+                alt={product.name}
+                className="product-image"
+              />
+              <h3 className="product-name">{product.name}</h3>
+              <p className="product-price">
+                <span className="current-price">₹{product.price}</span>
+              </p>
+              <p className="product-rating">
+                ⭐ {product.rating} ({product.reviews})
+              </p>
+            </div>
+          ))}
+        </div>
+      </Link>
     </div>
   );
 };
