3484469088234465235e4db0dc894502a30943fb
Tejas khope <khopetejas6@gmail.com>
2025-03-09 18:53:22 +0530

    updates

--- Frontend/src/Pages/Home/Home.jsx
+++ Frontend/src/Pages/Home/Home.jsx
@@ -4,13 +4,19 @@ import Category from "../../components/Category/Category";
 import TopFooter from "../../components/TopFooter/TopFooter";
 import TradingSteps from "../../components/TradingSteps/TradingSteps";
 import B2C_Category from "../../components/B2C_Category/B2C_Category";
+import "./Home.css"; // Import CSS for styling
 
 function Home() {
   return (
     <div>
       <Hero />
-      <Category />
-      <B2C_Category />
+      <div className="section">
+        <Category />
+      </div>
+      <div className="separator"></div> {/* Separator between sections */}
+      <div className="section">
+        <B2C_Category />
+      </div>
       <TradingSteps />
       <TopFooter />
     </div>
