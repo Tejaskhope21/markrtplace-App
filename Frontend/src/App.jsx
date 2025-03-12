f8883983267dc62c2fd9fe9e543847f926dd6ae3
YadneshUmredkar75 <yadneshumredkar75@gmail.com>
2025-03-12 12:09:40 +0530

    add a register pages

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,26 +1,32 @@
+import { useState, useEffect } from "react";
+import { Routes, Route, useNavigate } from "react-router-dom";
 import Navbar from "../src/components/Navbar/Navbar";
 import Footer from "../src/components/Footer/Footer";
 import Home from "../src/Pages/Home/Home";
-import "./App.css";
-import { useState } from "react";
 import Login from "../src/components/Login/Login";
-import { Routes, Route } from "react-router-dom";
 import Products from "./Pages/Products/Products";
-
 import Cart from "./Pages/Cart/Cart";
 import ShoppingProduct from "./Pages/ShoppingProduct/ShoppingProduct";
 import BuyNow from "./Pages/BuyNow/BuyNow";
 import Buy_B2C from "./Pages/Buy_B2C/Buy_B2C";
 import PlaceOrder from "./Pages/PlaceOrder/PlaceOrder";
+import Register from "./Pages/Register/Register";
+import "./App.css";
+
 function App() {
   const [showLogin, setShowLogin] = useState(false);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (showLogin) {
+      navigate("/register"); // Navigate to the /register route
+    }
+  }, [showLogin, navigate]);
 
   return (
     <>
-      {showLogin ? <Login /> : null}
-
       <div className="main-content">
-      <Navbar setShowLogin={setShowLogin} />
+        <Navbar setShowLogin={setShowLogin} />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/cart" element={<Cart />} />
@@ -29,13 +35,12 @@ function App() {
           <Route path="/buy" element={<BuyNow />} />
           <Route path="/buy_b2c" element={<Buy_B2C />} />
           <Route path="/placeorder" element={<PlaceOrder />} />
+          <Route path="/register" element={<Register />} />
         </Routes>
-      <Footer />
+        <Footer />
       </div>
-
     </>
   );
 }
 
 export default App;
-// div id="root"div
---
+++ Frontend/src/Pages/Register/Register.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Login from '../../components/Login/Login';
+import './Register.css'; // Import the CSS file
+
+function Register() {
+  return (
+    <div className="register-page">
+      <Login />
+    </div>
+  );
+}
+
+export default Register;
