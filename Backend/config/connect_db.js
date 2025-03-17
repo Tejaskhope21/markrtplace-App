8bb6d20334ed29330958c85f85cb36c3a960629c
YadneshUmredkar75 <yadneshumredkar75@gmail.com>
2025-03-17 23:36:58 +0530

    connect the mongoose

--- Backend/config/connect_db.js
+++ Backend/config/connect_db.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 import dotenv from "dotenv";
 dotenv.config();
 const connectDB = async () => {
-  await mongoose.connect().then(()=>{console.log("MongoDB Connected");})
+  await mongoose.connect(process.env.Mongo).then(()=>{console.log("MongoDB Connected");})
 
 }
 export default connectDB;
