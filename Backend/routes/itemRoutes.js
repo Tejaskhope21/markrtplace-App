5499f8d75f089e6af39c9fc094d8e807b83c968f
YadneshUmredkar75 <yadneshumredkar75@gmail.com>
2025-03-20 12:04:32 +0530

    create a upload folder

--- Backend/routes/itemRoutes.js
+++ Backend/routes/itemRoutes.js
@@ -1,12 +1,12 @@
 import express from 'express';
 import { getItems, getItemById, item_add, updateItem, deleteItem } from '../controllers/itemController.js';
-import upload from '../middleware/upload'; // Assuming you'll create this middleware
+import upload from '../middleware/uplode.js'; 
 
 const router = express.Router();
 
 router.get('/', getItems);
 router.get('/:id', getItemById);
-router.post('/add', upload.single('image'), item_add); // Handle single image upload
+router.post('/', upload.single('image'), item_add); // Change '/add' to '/'
 router.put('/:id', updateItem);
 router.delete('/:id', deleteItem);
 
